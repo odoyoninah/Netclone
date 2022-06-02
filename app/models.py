b0290cd67d736af3298d1dda307e68b93e18f5f3
odoyonina <odoyoninah@gmail.com>
2022-06-02 21:25:41 +0300

    install tkinter

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
@@ -8,7 +9,26 @@ AGE_CHOICES = (
     ('KIDS', 'KIDS'),
     ('ADULTS', 'ADULTS'),
 )
+genre = (
+    ('Action', 'Action'),
+    ('Horror', 'Horror'),
+    ('Animation', 'Animation'),
+    ('Comedy', 'Comedy'),
+    ('Family', 'Family'),
+)
 
+STATUS_CHOICES = (
+    ('Recently Added', 'Recently Added'),
+    ('Most Watched', 'Most Watched'),
+    ('Top Rated', 'Top Rated'),
+)
+
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age_group = models.CharField(max_length=10, choices=AGE_CHOICES, default='ALL')
 
 
 class Movie(models.Model):
@@ -16,6 +36,11 @@ class Movie(models.Model):
     description = models.TextField(blank=True, null=True)
     id = models.CharField(max_length=255, primary_key=True)
     year = models.IntegerField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    genre = models.CharField(max_length=255, blank=True, null=True)
+    video = models.FileField(upload_to='videos/', blank=True, null=True)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES)
+    
 
 
 class Profile(models.Model):
@@ -23,6 +48,11 @@ class Profile(models.Model):
     movies = models.ManyToManyField(Movie, related_name="watched_movies")
     name = models.CharField(max_length=255)
     age_limit = models.CharField(max_length=50,choices=AGE_CHOICES)
+    id = models.CharField(max_length=255, primary_key=True)
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     id = models.CharField(max_length=255, primary_key=True)
 
 
