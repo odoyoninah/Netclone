93de48b540afdefe975f6f7516fc9d86b40deea6
odoyonina <odoyoninah@gmail.com>
2022-06-03 05:13:41 +0300

    intergrated youtube api

--- app/requests.py
+++ app/requests.py
@@ -10,3 +10,14 @@ def get_movies(category):
         if movie['poster_path'] is not None:
             movie_list.append(movie)
     return movie_list
+
+def get_movie_details(movie_id):
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={config("API_KEY")}'
+    return requests.get(url).json()
+
+def you_tube_trailer(search_title):
+    search_query = search_title.replace(' ', '+')
+    url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={search_query}&key={config("YOU_TUBE_API_KEY")}'
+    response = requests.get(url)
+    youtube_id = response.json()['items'][0]['id']['videoId']
+    return youtube_id
--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from .requests import get_movies
+from .requests import get_movies,get_movie_details, you_tube_trailer
 
 # Create your views here.
 def index(request):
     popularMovies = get_movies('popular')
 
     return render(request, 'index.html', {'popularMovies': popularMovies})
-    
+
+def details(request, movie_id):
+    movie=get_movie_details(movie_id)
+    youtube_id = you_tube_trailer(movie['title'])
+    you_tube_url = f'https://www.youtube.com/embed/{youtube_id}?autoplay=1&muted=0'
+    return render(request, 'details.html', {'movie': movie,'you_tube_url':you_tube_url})
+
